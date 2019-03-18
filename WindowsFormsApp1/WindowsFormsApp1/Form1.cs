fe6ce76afa5adecb5fb242d682cf42dbaf14b57e
Stanley <30459659+Kumaken@users.noreply.github.com>
2019-03-18 16:59:16 +0700

    Animation done!

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using Microsoft.Glee.Drawing;
 using System.Drawing.Imaging;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace HideAndSeek
 {
@@ -123,7 +124,7 @@ namespace HideAndSeek
 
         }
 
-        //Setter & Getter from Form1 Class for Process Class:
+    //Setter & Getter from Form1 Class for Process Class:
         //Get External File URL:
         public String getFileURL()
         {
@@ -181,19 +182,47 @@ namespace HideAndSeek
     static class Process
     {
         static List<List<int>> tree;
+        static int[] pointsTo;
         static bool[] visited;
         static long[] arrive;
         static long[] leave;
         static long globaleZeit = 1;
+        static List<int> Path = new List<int>();
+        //Create graph object
+        static Microsoft.Glee.Drawing.Graph defaultG = new Microsoft.Glee.Drawing.Graph("graph");
+        // Creating form for graph visualization
+        static System.Windows.Forms.Form graphForm = new System.Windows.Forms.Form();
+        // Create object GViewer to view the constructed graph
+        static Microsoft.Glee.GraphViewerGdi.GViewer viewer = new Microsoft.Glee.GraphViewerGdi.GViewer();
         //[STAThread]
-        static void buildTree(ref List<List<int>> tree)
+        static void buildTree(ref StreamReader sr, int jmlRumah)
         {
-            tree.RemoveAt(0);
+            tree = new List<List<int>>(jmlRumah + 1);
+            pointsTo = new int[jmlRumah + 1];
+            for (int i = 0; i <= jmlRumah; i++)
+            {
+                tree.Add(new List<int>());
+            }
+
+            for (int i = 0; i < jmlRumah - 1; i++)
+            {
+                string[] line = sr.ReadLine().Split();
+
+                int a = Int32.Parse(line[0]);
+                int b = Int32.Parse(line[1]);
+                //Console.WriteLine(jmlRumah);
+                pointsTo[b] = a;
+                tree[a].Add(b);
+                tree[b].Add(a);
+                // Add edges between 2 vertices to graph object:
+                Microsoft.Glee.Drawing.Edge e = defaultG.AddEdge(line[0], line[1]);
+                // Set edges without arrowhead => UNDIRECTED GRAPH!
+                e.Attr.ArrowHeadAtTarget = ArrowStyle.None;
+            }
         }
 
         static void DFS(int startNode, ref List<List<int>> tree)
         {
-            Console.WriteLine(startNode);
             visited[startNode] = true;
             arrive[startNode] = globaleZeit++;
             foreach (var childNode in tree[startNode])
@@ -210,89 +239,153 @@ namespace HideAndSeek
         {
             return arrive[nodeChild] > arrive[nodeParent] && leave[nodeChild] < leave[nodeParent];
         }
+        // Generate Path:
+        static public void generatePath(int bottom, int upper, bool reverse)
+        {
+            Path.Clear();
+            int current = bottom;
+            while (current != pointsTo[upper])
+            {
+                Path.Add(current);
+                current = pointsTo[current];
+            }
+            if (reverse)
+            {
+                Path.Reverse();
+            }
+        }
+        // Uncolor Graph:
+        public static void resetGraph()
+        {
+            for (int i = 0; i < Path.Count; i++)
+            {
+                // COLOR THE NODES TRAVERSED IN RED
+                if(Path[i].ToString() == "1")
+                {
+                    Microsoft.Glee.Drawing.Node n = defaultG.FindNode(Path[i].ToString());
+                    n.Attr.Fillcolor = Microsoft.Glee.Drawing.Color.SeaGreen;
+                }
+                else
+                {
+                    Microsoft.Glee.Drawing.Node n = defaultG.FindNode(Path[i].ToString());
+                    n.Attr.Fillcolor = Microsoft.Glee.Drawing.Color.White;
+                }
+                
+            }
+        }
+        // Deepcopy Object Function:
+        public static T DeepClone<T>(T obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+        // Colorize edges and vertices to mark path:
+        public static void updateGraph()
+        {
 
-     // FUNCTION TO DO LOAD GRAPH AND RUN DFS:
+            for (int i = 0; i < Path.Count; i++)
+            {
+                // COLOR THE NODES TRAVERSED IN RED
+                Node n = defaultG.FindNode(Path[i].ToString());
+                n.Attr.Fillcolor = Microsoft.Glee.Drawing.Color.DarkRed;
+                // Bind the graph to the viewer
+                viewer.Graph = defaultG;
+                viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+                graphForm.Refresh();
+                System.Threading.Thread.Sleep(500);
+                // COLOR THE EDGES TOO:
+                // Edge e = graph.fi
+                // e.Attr.ArrowHeadAtTarget = ArrowStyle.None;
+            }
+            // Bind the graph to the viewer
+            //viewer.Graph = defaultG;
+
+            /*
+            // Associate the viewer with the created form
+            graphForm.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            graphForm.Controls.Add(viewer);
+            graphForm.ResumeLayout();
+
+            // Show the form as a new window, BUT not locked there!
+            graphForm.Show();*/
+
+        }
+
+        // FUNCTION TO DO LOAD GRAPH AND RUN DFS:
         public static void runDFS(Form1 f1)
         {
-            //Creating form for graph visualization
-            System.Windows.Forms.Form graphForm = new System.Windows.Forms.Form();
-            //Create object GViewer to view the constructed graph
-            Microsoft.Glee.GraphViewerGdi.GViewer viewer = new Microsoft.Glee.GraphViewerGdi.GViewer();
-            //Create graph object
-            Microsoft.Glee.Drawing.Graph graph = new Microsoft.Glee.Drawing.Graph("graph");
-     
-            tree = new List<List<int>>();
 
             StreamReader sr = new StreamReader(@f1.getFileURL());
             string Rumah = sr.ReadLine();
-
             int jmlRumah = Int32.Parse(Rumah);
 
+            buildTree(ref sr, jmlRumah);
 
-            tree = new List<List<int>>(jmlRumah + 1);
-            for (int i = 0; i <= jmlRumah; i++)
-            {
-                tree.Add(new List<int>());
-            }
-
-            for (int i = 0; i < jmlRumah - 1; i++)
-            {
-                string[] line = sr.ReadLine().Split();
-
-                int a = Int32.Parse(line[0]);
-                int b = Int32.Parse(line[1]);
-                tree[a].Add(b);
-                tree[b].Add(a);
-                //Add edges between 2 vertices to graph object:
-                Microsoft.Glee.Drawing.Edge e = graph.AddEdge(line[0], line[1]);
-                //Set edges without arrowhead => UNDIRECTED GRAPH!
-                e.Attr.ArrowHeadAtTarget = ArrowStyle.None;
-            }
-
-            //DFS
+            // DFS
             arrive = new long[jmlRumah + 1];
             leave = new long[jmlRumah + 1];
             visited = new bool[jmlRumah + 1];
 
             DFS(1, ref tree);
 
-            //COLOR THE ROOT (1) WITH SEAGREEN COLOR
-            Microsoft.Glee.Drawing.Node n1 = graph.FindNode("1");
+            // COLOR THE ROOT (1) WITH SEAGREEN COLOR
+            Microsoft.Glee.Drawing.Node n1 = defaultG.FindNode("1");
             n1.Attr.Fillcolor = Microsoft.Glee.Drawing.Color.SeaGreen;
-            //Make Root a double-circled vertex just for emphasis, hehe:
+            // Make Root a double-circled vertex just for emphasis, hehe:
             n1.Attr.Shape = Microsoft.Glee.Drawing.Shape.DoubleCircle;
 
-            //SHOWING THE GRAPH:
-            //Bind the graph to the viewer
-            viewer.Graph = graph;
+            // SHOWING THE GRAPH:
+            // Bind the graph to the viewer
+            viewer.Graph = defaultG;
 
-            //Associate the viewer with the created form
+            // Associate the viewer with the created form
                 graphForm.SuspendLayout();
                 viewer.Dock = System.Windows.Forms.DockStyle.Fill;
                 graphForm.Controls.Add(viewer);
                 graphForm.ResumeLayout();
 
-            //Show the form as a new window, BUT not locked there!
+            // Show the form as a new window, BUT not locked there!
                 graphForm.Show();
         }
 
     // FUNCTION TO DO QUERY:
         public static void runQuery(Form1 f1)
         {
-            if (f1.get01() != "" || f1.geta() != "" || f1.getb() != "")
+
+            if (f1.get01() != "" || f1.geta() != "" || f1.getb() != "" || f1.getFileURL() != "")
             {
-                //Parse input (string) into integer from textboxes.
+                // Parse input (string) into integer from textboxes.
                 int Q = Int32.Parse(f1.get01());
                 int _a = Int32.Parse(f1.geta());
                 int _b = Int32.Parse(f1.getb());
 
-                //Check for answer:
+                // ResetGraph First:
+                if (Path != null)
+                {
+                    resetGraph();
+                    viewer.Graph = defaultG;
+                    viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+                    graphForm.Refresh();
+
+                }
+
+                // Check for answer:
                 if (Q == 1)
                 {
                     if (isChildOf(_a, _b))
                     {
                         f1.displayQans(1);
                         f1.changePictureYES();
+                        // Generate Path:
+                        generatePath(_a, _b, true);
+                        // Update Graph:
+                        updateGraph();
                     }
                     else
                     {
@@ -306,6 +399,10 @@ namespace HideAndSeek
                     {
                         f1.displayQans(1);
                         f1.changePictureYES();
+                        // Generate Path:
+                        generatePath(_b, _a, false);
+                        // Update Graph:
+                        updateGraph();
                     }
                     else
                     {
@@ -316,6 +413,7 @@ namespace HideAndSeek
             }
             
         }
+
     }
 
 
